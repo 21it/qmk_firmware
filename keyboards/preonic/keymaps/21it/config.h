11f0b5037b842c350edd395902e23718845ff1a2
tkachuk-labs <tkachuk.labs@gmail.com>
2022-05-21 23:02:00 +0300

    fix sound

--- keyboards/preonic/keymaps/21it/config.h
+++ keyboards/preonic/keymaps/21it/config.h
@@ -17,7 +17,11 @@
 #pragma once
 
 #ifdef AUDIO_ENABLE
-#    define STARTUP_SONG SONG(PREONIC_SOUND)
+#    define AUDIO_INIT_DELAY
+#    define AUDIO_ENABLE_TONE_MULTIPLEXING
+#    define AUDIO_TONE_MULTIPLEXING_RATE_DEFAULT 10
+#    define STARTUP_SONG SONG(STARTUP_SOUND)
+#    define PITCH_STANDARD_A 110.0f
 // #define STARTUP_SONG SONG(NO_SOUND)
 
 #    define DEFAULT_LAYER_SONGS \
@@ -42,7 +46,7 @@
    - Virtual sustain, portamento, and modulation wheel
    - etc.
 */
-//#define MIDI_ADVANCED
+#define MIDI_ADVANCED
 
 /* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
 //#define MIDI_TONE_KEYCODE_OCTAVES 2
