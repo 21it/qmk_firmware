03cb5694cf78d7907f6d5c4b145ddda0fbd56098
tkachuk-labs <tkachuk.labs@gmail.com>
2022-07-05 23:20:41 +0300

    q4 first row

--- keyboards/keychron/q4/q4_ansi_stm32l432/keymaps/21it/keymap.c
+++ keyboards/keychron/q4/q4_ansi_stm32l432/keymaps/21it/keymap.c
@@ -123,7 +123,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
         MO(_FN1),KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,            KC_ENT,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,            KC_ENT,
-        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT,  MO(_FN2), MO(_FN3), KC_RCTL),
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             MO(_FN3), KC_RGUI,  KC_ALGR,  KC_RCTL),
 
     [_FN1] = LAYOUT_ansi_61(
         MO(_FN3),_______, _______, _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,   _______,  _______,  _______,
