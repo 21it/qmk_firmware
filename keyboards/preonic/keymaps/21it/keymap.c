98874c229c590fd8b4549fccb7ab7a8280763ced
tkachuk-labs <tkachuk.labs@gmail.com>
2022-09-15 20:45:50 +0300

    fix scrolling

--- keyboards/preonic/keymaps/21it/keymap.c
+++ keyboards/preonic/keymaps/21it/keymap.c
@@ -165,7 +165,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   MU_TOG,  KC_BRID, KC_BRIU, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, EM_SRG,
   _______, KC_F1  , KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_BTN4, KC_BTN1, KC_BTN2, KC_BTN3, EM_FIN,  EM_LEN,
   _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, EM_FIX,  EM_FLP,
-  _______, KC_F11,  KC_F12,  KC_PSCR, KC_SCRL, KC_PAUS, KC_WH_L, KC_WH_U, KC_WH_D, KC_WH_R, _______, _______,
+  _______, KC_F11,  KC_F12,  KC_PSCR, KC_SCRL, KC_PAUS, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET
 )
 
